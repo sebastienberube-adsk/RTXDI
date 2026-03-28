6128ed7805f5c94ea9738b39cc1b4f63c5e19044
Sebastien Berube <sebastien.berube@autodesk.com>
2026-03-28 08:53:35 -0400

    Custom scene for IntermediateSample

--- Samples/IntermediateSample/Source/Testing.cpp
+++ Samples/IntermediateSample/Source/Testing.cpp
@@ -193,6 +193,7 @@ void ProcessCommandLine(int argc, char** argv, donut::app::DeviceCreationParamet
         ("render-height", "Internal render target height, overrides window size", value(args.renderHeight))
         ("save-file", "Save frame to file and exit", value(args.saveFrameFileName))
         ("save-frame", "Index of the frame to save, default is 0", value(args.saveFrameIndex))
+        ("scene", "Scene file path (VFS path, e.g. /Assets/Media/arcade.scene.json or /Assets/Media/Arcade/Arcade.gltf)", value(args.scenePath))
         ("tone-mapping", "Tone mapping toggle", value(ui.enableToneMapping))
         ("transparent", "Transparent materials toggle", value(ui.gbufferSettings.enableTransparentGeometry))
         ("verbose", "Enable debug log messages", value(args.verbose))

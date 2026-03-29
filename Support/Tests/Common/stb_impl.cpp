1dbc9aacb1196eccb533ebae7fd7d542ac44c9c2
Sebastien Berube <sebastien.berube@autodesk.com>
2026-03-29 17:34:38 -0400

    Add MinimalSample_SelfComparison, refine threshold

--- Support/Tests/Common/stb_impl.cpp
+++ Support/Tests/Common/stb_impl.cpp
@@ -1,4 +1,6 @@
-// Provides stb_image implementation for standalone test executables
-// that don't link against donut_engine (which has its own stb_image impl).
+// Provides stb_image / stb_image_write implementations for standalone test
+// executables that don't link against donut_engine (which has its own copy).
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
